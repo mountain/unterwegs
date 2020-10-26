88f797025f9396fd16552adc80807fd0ed3ebb84
Mingli Yuan <mingli.yuan@gmail.com>
2020-10-27 01:00:40 +0800

    fix indexer

--- unterwegs/tasks/indexer.py
+++ unterwegs/tasks/indexer.py
@@ -52,7 +52,7 @@ def init_index():
                     'facet': True
                 },
             ],
-            'default_sorting_field': 'title'
+            'default_sorting_field': 'pubdate'
         })
     if 'pages' not in cs:
         client.collections.create({
@@ -75,7 +75,7 @@ def init_index():
                   'type': 'string',
                 }
             ],
-            'default_sorting_field': 'article'
+            'default_sorting_field': 'index'
         })
 
 
