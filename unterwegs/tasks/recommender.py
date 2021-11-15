3ff949afbd2ea833d4878479098828ec68a95417
mingli <mingli.yuan@gmail.com>
2021-11-15 22:59:49 +0800

    add recommend pages

--- unterwegs/tasks/recommender.py
+++ unterwegs/tasks/recommender.py
@@ -17,17 +17,19 @@ def index(pid, page_content):
     get_task_logger('recommender').info('start %s' % pid)
 
     encoded = quote_plus(page_content)
-    json = requests.get('http://%s:%s/topics/%s' % (lookup("lda"), '7777', encoded)).json()
-    if 'topics' in json:
-        vec = josn['topics']
+    resp = requests.get('http://%s:%s/topics/%s' % (lookup("lda"), '7777', encoded))
+    if resp.status_code == 200:
+        data = resp.json()
+        if 'topics' in data:
+            vec = data['topics']
 
-        if not ri.hexists('pid2vid', pid):
-            vid = ri.incr('serial:vid')
-            ri.hset('pid2vid', pid, vid)
-            ri.hset('vid2pid', vid, pid)
-        else:
-            vid = ri.hget('pid2vid', pid)
+            if not ri.hexists('pid2vid', pid):
+                vid = ri.incr('serial:vid')
+                ri.hset('pid2vid', pid, vid)
+                ri.hset('vid2pid', vid, pid)
+            else:
+                vid = ri.hget('pid2vid', pid)
 
-        vid = int(vid)
-        sb.execute_command('vadd', 'page', vid, *vec)
-        get_task_logger('recommender').info('finish %s' % pid)
+            vid = int(vid)
+            sb.execute_command('vadd', 'page', vid, *vec)
+            get_task_logger('recommender').info('finish %s' % pid)
